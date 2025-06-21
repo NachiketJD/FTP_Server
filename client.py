8505d46f218a2d927710948b0284ae0ec96881a0
Nachiket Jayant Deshpande <nachiketdeshpande2004@gmail.com>
2025-06-22 00:41:15 +0530

    LOOn

--- client.py
+++ client.py
@@ -8,3 +8,12 @@ if __name__ == "__main__":
    s = socket.socket(socket.AF_INET, socket.SOCK_STREAMS) # TCP/IP 
    s.bind((host,port))
    s.listen(totalClients)
+
+   connections = []
+   print("Initiating Clinets")
+
+   while True:
+      for i in range (totalClients):
+         conn = s.accept()
+         connections.append(conn)
+         print("Connected with client", i+1)
